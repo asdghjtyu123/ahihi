a808cc4983d3d96683433f07b061411b700e5dfb
huyhoang <asdghjtyu12@gmail.com>
2023-01-02 01:17:32 +0700

    seq 1.0

---
+++ seq2seq.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from torchtext.datasets import Multi30k
+from torchtext.data import Field, TabularDataset
+
+import spacy
+import numpy as np
+
+import random
+import math
+import time
+
+spacy_de = spacy.load('de_core_news_sm')
+spacy_en = spacy.load('en_core_web_sm')
