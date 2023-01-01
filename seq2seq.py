37e40fb65cfe7715e5d0b55cc59dc31b6eb0bb4a
huyhoang <asdghjtyu12@gmail.com>
2023-01-02 01:53:18 +0700

    seq 2.0

--- seq2seq.py
+++ seq2seq.py
@@ -14,3 +14,6 @@ import time
 
 spacy_de = spacy.load('de_core_news_sm')
 spacy_en = spacy.load('en_core_web_sm')
+def tokenize_de(text):
+    
+    return [tok.text for tok in spacy_de.tokenizer(text)]
